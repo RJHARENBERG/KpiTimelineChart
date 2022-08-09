f16d60c65ca34494b70f519c27b47169ff90319b
RJHARENBERG <reinardharenberg@gmail.com>
2022-08-09 10:27:53 +0200

    first commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var kpiTimelineChart1B602700AB3F4033A60525DA22A0AD06_DEBUG: IVisualPlugin;
+export default kpiTimelineChart1B602700AB3F4033A60525DA22A0AD06_DEBUG;
